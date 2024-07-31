a638f180165388a81ed369518bab104582273e40
Prasanna <prasu@Prasannas-MacBook-Pro.local>
2024-07-31 09:06:27 -0700

    Add brand-Hero component

---
+++ src/components/branding/brand-hero/brand-hero.tsx
@@ -0,0 +1,8 @@
+export default function BrandHero() {
+  return (
+    <div className="flex">
+        <div className="flex-auto border w-1/2">test</div>
+        <div className="flex-auto border w-1/2">test</div>
+    </div>
+  )
+}
