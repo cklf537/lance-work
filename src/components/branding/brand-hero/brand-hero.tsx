7f322a03eb1192b959f5ef77c3e0465f0031b5c8
Prasanna <prasu@Prasannas-MacBook-Pro.local>
2024-08-03 13:48:25 -0700

    update state Object

--- src/components/branding/brand-hero/brand-hero.tsx
+++ src/components/branding/brand-hero/brand-hero.tsx
@@ -19,20 +19,30 @@ export default function BrandHero() {
         category?: CATEGORY;
     }
 
-    interface Action{
+    interface Action extends INITIALSTATE<USER, CATEGORY>{
         type?:string;
-        payload?: {}
+        payload?: INITIALSTATE<USER, CATEGORY>
     }
 
-    const initialState: INITIALSTATE<USER, CATEGORY> = {};
+    const initialState: INITIALSTATE<USER, CATEGORY> = {
+        user:{
+            name:"Admin",
+            user_id: 1
+        },
+        category:{
+            name: "Admin",
+            category_id:1
+        }
+    };
 
     function reducer(state: INITIALSTATE<USER, CATEGORY>, action: Action): INITIALSTATE<USER, CATEGORY> {
+        // const {user, category} = action.payload
         switch(action.type){
             case "INCREMENT":
                 return{
                     ...state,
-                    user: {name:"", user_id:1},
-                    category: action.payload
+                    user: action.payload?.user,
+                    category: action.payload?.category
                 }
             default:
                 throw new Error();          
@@ -45,14 +55,17 @@ export default function BrandHero() {
         <div className="lg:flex p-5">
             <div className={`flex lg:w-1/2 lg:h-1/2 p-5 lg:px-0 justify-center bg-no-repeat min-h-96 bg-auto bg-bottom max-h-dvh rounded-br-lg rounded-tl-lg`} style={{backgroundImage: `url(${img1})`}}>
             </div>
-            {state && state.category != undefined ? state.category.name : ""}
+            {`${state && state.category?.name}${state.category?.category_id}`}
             <div className="flex lg:w-1/2 p-5 lg:px-0 items-end">
                 <div className="pl-8">
                     <h1 className="text-5xl font-bold pb-5">Refferal,</h1>
                     <h3 className="text-3xl pb-5">Drives Industries with top talent.</h3>
                     <button className="p-2 px-8 text-1xl rounded-full mr-5 bg-purple-600 text-white" onClick={()=>{dispatch({
                         type: "INCREMENT",
-                        payload: {name:"chandra", category_id:1}
+                        payload: {
+                            user:{name: "User Name",  user_id: 1},
+                            category:{name: "USER", category_id:1}
+                        }
                     })}}>Reffer</button>
                     <button className="p-2 px-8 text-1xl rounded-full bg-purple-600 text-white">Find refferal</button>
                 </div>
