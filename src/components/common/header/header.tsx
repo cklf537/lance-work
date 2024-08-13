d3136e7cc569a07bb61b56081419d6a4f67bdb66
clife537 <chandu537@gmail.com>
2024-08-13 11:06:49 -0700

    Update main navigation

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -2,32 +2,32 @@ import { Link } from "react-router-dom";
 import Search from "../search/search";
 import { AppState, Category, Groups, NavItems, Users } from "../../../data/model";
 import Navigation from "../navigation/navigation";
-import LevelTwoNavigation from "../navigation/level-two-navigation";
+import { useState } from "react";
 
+export default function Header(heading: AppState<Users, Groups, NavItems, Category>) {
+  const collapseMenu: boolean = false;
+  const [menuStatus, setMenuStatus] = useState(collapseMenu);
 
-  export default function Header(heading: AppState<Users, Groups, NavItems, Category>) {
-    return (
+  return (
     <div className="">
       <div className="container mx-auto h-20 flex px-5 lg:px-0 md:sm:justify-items-center sm:md:items-center sm:md:justify-center">
         <div className="flex flex-auto gap-8  content-center items-center justify-start">
           <h1 className='text-1xl font-bold text-purple-600'>
             <Link to={`${'\/'}`}>Lance-work</Link>
-            </h1>
-            <Navigation heading={heading}>
-              {/* <LevelTwoNavigation /> */}
-            </Navigation>
+          </h1>
+          <Navigation 
+          heading={heading} 
+          menuStatus={menuStatus} 
+          setMenuStatus={setMenuStatus}
+          ></Navigation>
         </div>
-        <div className="flex  gap-8 content-center group items-center justify-end text-sm">
-          {/* <div className="flex place-content-end items-center text-sm"> */}
-            <Search />
-            <div className="" key={`${heading.heading &&  heading.heading[4].id}`}>
-                <Link to={`/signup`}> {heading.heading &&  heading.heading[4].title} </Link>
-                </div>
-            <span className="material-symbols-outlined hover:group-hover" >menu</span>
-          {/* </div> */}
-        <div className="w-full   invisible group-hover:visible">This sub navigationasjdlkajsldkalsjd asjdajsdl asdjlajsdl alsdjlasjdlk asdljalskjd</div>
+        <div className="flex gap-8 content-center items-center justify-end text-sm">
+          <Search />
+          <div className="" key={`${heading.heading && heading.heading[4].id}`}>
+            <Link to={`/signup`}> {heading.heading && heading.heading[4].title} </Link>
+          </div>
+          <span className="material-symbols-outlined hover:group-hover" >menu</span>
         </div>
-        
       </div>
     </div>
   );
--- src/components/common/navigation/navigation.tsx
+++ src/components/common/navigation/navigation.tsx
@@ -1,32 +1,31 @@
-import { ReactNode, useState } from "react";
+import { Dispatch, ReactNode, SetStateAction } from "react";
 import { AppState, Category, Groups, NavItems, Users } from "../../../data/model";
 import { Link } from "react-router-dom";
 
 export interface Props {
   children?: ReactNode;
   heading: AppState<Users, Groups, NavItems, Category>;
+  menuStatus: boolean;
+  setMenuStatus: Dispatch<SetStateAction<boolean>>
 };
 
-export default function Navigation({children, heading}: Props) {
-  const [isOpen, setIsOpen] = useState(false);
-
- 
-
+export default function Navigation({children, heading, menuStatus, setMenuStatus}: Props) {
   return (
-    <div className="group">
+    <div className="">
         <nav className='hidden lg:flex lg:gap-8 lg:item-center lg:text-sm lg:block'>
             {heading.heading && heading.heading.map((nav, i) => i <= 3 ? <div className='' key={nav.id}>
-            <Link to={`${nav.link}`}>{nav.title}</Link>
+            <Link to={`${nav.link}`} onMouseOver={()=>{setMenuStatus(true)}}>{nav.title}</Link>
             </div> : "")}
           </nav>
-          <section className="border bg-white p-5 w-full absolute h-96 top-24 left-0 invisible group-hover:visible" >
+          <section className={`border bg-white p-5 w-full absolute h-96 top-24 left-0 ${menuStatus == false ? `invisible` : `visible`}`} 
+          onMouseOver={()=>{setMenuStatus(true)}} 
+          onMouseOut={()=>{setMenuStatus(false)}} >
             <div className="container mx-auto">
             <div>Level 2.1</div>
             <div>Level 2.1</div>
             <div>Level 2.1</div>
             <div>Level 2.1</div>
             <div>Level 2.1</div>
-            {/* {expandMenue<compState>({})} */}
             </div>
           </section>
         {children && children}
